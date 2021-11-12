56eb5262b27adac6f188d68fd29c4b99452d07ff
chakrireddy1609 <chakrireddy1609@gmail.com>
2021-11-12 11:08:57 +0530

    new commits

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("From separate file");
+console.log("From separate file again");
+let name = 'chakri';
+let salary = 35.5;
+let user = {
+
+    name : "Chakri",
+    age : 32,
+    designation : "QA"
+
+};
+console.log(user);
