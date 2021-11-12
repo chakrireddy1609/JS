e8eba854383156d143e3fc2a36af8bcb8f8044bc
chakrireddy1609 <chakrireddy1609@gmail.com>
2021-11-12 12:14:18 +0530

    JS Basics

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ let user = {
 
 };
 console.log(user);
+
+let languages = ['Python', 'JS', 'Java'];
+console.log(languages[1]);
+languages[1] = 'Ruby';
+console.log(languages[1]);
+
+function addition(num1,num2){
+
+    return num1+num2;
+
+}
+
+let result = addition(13,34);
+console.log(result)
+
+function square(num1){
+
+    return num1*num1;
+}
+
+let square_result = square(9);
+console.log(square_result)
